23d0688dca4ee1d4640a25322f957d4906018007
alexweishao <1633014815@qq.com>
2022-07-26 14:05:50 +0800

    login&register逻辑分离

--- working/mini_blog/routers/router.go
+++ working/mini_blog/routers/router.go
@@ -2,22 +2,21 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
-	"mini_blog/controller"
+	"mini_blog/controllers"
 )
 
 func ParentRouter(router *gin.Engine) {
-	router.GET("/", controller.ToRegister) //默认接口为注册页面
+	//router.GET("/", controllers.ToRegister) //默认接口为注册页面
 
-	router.GET("/to_login", controller.ToLogin)  //登录接口
-	router.POST("/do_login", controller.DoLogin) //登录接口
-	router.GET("/redirect", controller.Redirect)
+	router.GET("/login", controllers.LoginGet)   //登录GET接口
+	router.POST("/login", controllers.LoginPost) //登录接口
 
-	router.GET("/to_register", controller.ToRegister)  //登录接口
-	router.POST("/do_register", controller.DoRegister) //注册接口
+	router.GET("/register", controllers.RegisterGet)   //注册GET接口
+	router.POST("/register", controllers.RegisterPost) //注册POST接口
 
-	//router.GET("/user_index_page", controller.UserIndexPage) //注册接口
-	//router.GET("/publish_message", controller.PublishMessage)            //发布消息
-	//router.GET("/all_message_list", controller.AllMessageList)           //全部消息列表
-	//router.GET("/personal_message_list", controller.PersonalMessageList) //个人消息列表
-	//router.GET("/message_detail", controller.MessageDetail)              //消息详情
+	//router.GET("/user_index_page", controllers.UserIndexPage) //注册接口
+	//router.GET("/publish_message", controllers.PublishMessage)            //发布消息
+	//router.GET("/all_message_list", controllers.AllMessageList)           //全部消息列表
+	//router.GET("/personal_message_list", controllers.PersonalMessageList) //个人消息列表
+	//router.GET("/message_detail", controllers.MessageDetail)              //消息详情
 }
